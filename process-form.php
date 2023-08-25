3fd725607718b7f59fc877041614df5ce5b390af
Abdikhani <136441727+AbdyKhanY@users.noreply.github.com>
2023-08-25 15:38:31 +0300

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -1,16 +1,38 @@
 <?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    // Validate and sanitize data if needed.
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer;
     
-    $to = "wanagdon2@gmail.com"; // Your Gmail address
-    $subject = "New Contact Form Submission from $name";
-    $headers = "From: $email\r\n";
+    // SMTP configuration for Gmail
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'abdykhan.edu@gmail.com'; // Replace with your Gmail email
+    $mail->Password = 'kaaraan4'; // Replace with your Gmail password
+
+    // Sender and recipient
+    $mail->setFrom($email, $name);
+    $mail->addAddress('abdykhan.edu@gmail.com'); // Replace with your desired recipient email
+    
+    // Email subject and body
+    $mail->Subject = "New Contact Form Submission from $name";
+    $mail->Body = $message;
     
     // Send the email
-    mail($to, $subject, $message, $headers);
+    if ($mail->send()) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email sending failed: " . $mail->ErrorInfo;
+    }
+} else {
+    echo "Invalid request";
 }
 ?>
